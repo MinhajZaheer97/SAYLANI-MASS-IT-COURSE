33431906b24d8ba021fe4337b35c313407857b1e
Minhaj <minusahab69@gmail.com>
2026-01-10 20:37:24 +0500

    chapyer 6-9

---
+++ Documents/SMIT COURSE/MODULE 2/java script/chapter 6-9/app.js
@@ -0,0 +1,104 @@
+
+var a = 10;
+
+document.write("Result:<br>");
+
+
+document.write("The value of a is: " + a + "<br>");
+
+
+document.write("<hr>");
+
+
+
+
+document.write("The value of ++a is: " + (++a) + "<br>");
+
+
+
+document.write("Now the value of a is: " + a + "<br><br>");
+
+document.write("The value of a++ is: " + (a++) + "<br>");
+
+
+document.write("Now the value of a is: " + a + "<br><br>");
+
+
+document.write("The value of --a is: " + (--a) + "<br>");
+
+
+document.write("Now the value of a is: " + a + "<br><br>");
+
+
+document.write("The value of a-- is: " + (a--) + "<br>");
+
+
+document.write("Now the value of a is: " + a + "<br>");
+
+            //    question number 2
+
+document.write("<br>  <br>  <br>  <br> ")
+
+document.write("<hr>")
+
+document.write("<h1>Qno.2</h1>")
+
+document.write("<br>")
+
+var a = 2;
+var b = 1;
+
+var result = --a - --b + ++b + b--;
+
+document.write("a = 2 , b = 1");
+document.write("<br>");
+document.write("--a - --b + ++b + b--  =   " + result);
+
+document.write("<br>")
+
+document.write("<br>")
+
+document.write("Explanation")
+
+
+
+document.write("<br>")
+
+var a = 2;
+var b = 1;
+
+document.write("<br>")
+
+document.write("--a = " + --a)
+
+document.write("<br>")
+
+var a = 2;
+var b = 1;
+
+document.write("--a - --b = " + (--a - --b))
+
+document.write("<br>")
+
+var a = 2;
+var b = 1;
+
+document.write("--a - --b + ++b = " + (--a - --b + ++b))
+
+document.write("<br>")
+
+var a = 2;
+var b = 1;
+
+document.write("--a - --b + ++b + b-- = " + (--a - --b + ++b + b--));
+
+                    //    question number 3
+
+
+var name = prompt("enter your name")   
+
+alert("ASSALAMUALAIKUM " + name)
+
+                        //  question number 5
+
+                        
