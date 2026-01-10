895008d42e70ac731f934de3f8ef6da04ad85392
Minhaj <minusahab69@gmail.com>
2026-01-10 20:47:55 +0500

    ..

---
+++ Documents/SMIT COURSE/MODULE 2/java script/question number 5 of ch 6-9/app.js
@@ -0,0 +1,17 @@
+alert("ASSALAMUALAIKUM");
+
+      alert("WANT TO GENERATE A TABLE");
+
+      var number = Number(prompt("enter an number for table"));
+
+      var lenth = Number(prompt("enter the table length for table"));
+
+      document.write("<div style='text-align:center;'>");
+
+      document.write("<h1>TABLE OF " + number + " </h1>" + "<br>" + "<hr>");
+
+      for (var i = 1; i <= lenth; i++) {
+        document.write(number + " X " + i + " = " + number * i + "<hr>");
+      }
+
+      document.write("</div>");
