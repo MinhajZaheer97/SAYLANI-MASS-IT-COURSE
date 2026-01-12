eb8c424c20b98cff021839b3c5e2fff18e2a8421
Minhaj <minusahab69@gmail.com>
2026-01-12 22:14:18 +0500

    ch 9-10 qno 1

---
+++ Documents/SMIT COURSE/MODULE 2/java script/Ch 9-10;/Qno 1/app.js
@@ -0,0 +1,18 @@
+var city = "karachi"
+var cityname = prompt("enter the city name").toLowerCase();
+
+
+while(city !== cityname){
+    alert("bhagh ja be yahan se .............. \n \n( this web site is only available for karachi users)")
+    cityname = prompt("enter the city name").toLowerCase();
+}
+
+    alert("welcome to the city of lights");
+
+// if(city === cityname){
+//     alert("welcome to the city of lights");
+// }else{
+//     alert("bhagh ja be yahan se")
+// }
+
+
