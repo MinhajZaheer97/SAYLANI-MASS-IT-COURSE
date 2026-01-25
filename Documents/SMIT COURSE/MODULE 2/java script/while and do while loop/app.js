8f5942446607fa62c1338f3e99f368c255803353
Minhaj <minusahab69@gmail.com>
2026-01-25 19:19:36 +0500

    food panda is updated

---
+++ Documents/SMIT COURSE/MODULE 2/java script/while and do while loop/app.js
@@ -0,0 +1,31 @@
+        //  for loop
+// let num = +prompt("enter the number");
+// let power = +prompt("enter the power")
+// let result = num
+
+// for (i = 1 ; i < power ; i++){
+//     result = result * num;
+    
+// }
+
+// console.log(result);
+
+                // while loop
+
+
+// let num = +prompt("enter the number");
+// let power = +prompt("enter the power")
+// let result = num
+// var i = 1;
+
+// while(i < power){
+//     result = result * num;
+//     i++
+// }
+// console.log(result);
+
+
+                    // do while loop
+
+
+
