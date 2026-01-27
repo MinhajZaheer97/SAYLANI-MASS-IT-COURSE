55920e1a663efe4474bb1beab2c55372b979befe
Minhaj <minusahab69@gmail.com>
2026-01-27 22:07:35 +0500

    a

--- Documents/SMIT COURSE/MODULE 2/java script/SMIT ASSIGNMENTS/date and time assignment/app.js
+++ Documents/SMIT COURSE/MODULE 2/java script/SMIT ASSIGNMENTS/date and time assignment/app.js
@@ -1,12 +1,12 @@
-var userage = prompt('enter your date of birth')
-var age = new Date(userage)
-var today = new Date()
+var userage = prompt("enter your date of birth");
+var age = new Date(userage);
+var today = new Date();
 
-var msAge = age.getTime()
-var msToday = today.getTime()
+var msAge = age.getTime();
+var msToday = today.getTime();
 
 var msexactage = msToday - msAge;
- 
+
 var msexactageS = Math.floor(msexactage / 1000);
 var msexactageMi = Math.floor(msexactageS / 60);
 var msexactageH = Math.floor(msexactageMi / 60);
@@ -14,10 +14,26 @@ var msexactageD = Math.floor(msexactageH / 24);
 var msexactageM = Math.floor(msexactageD / 30.44);
 var msexactagey = Math.floor(msexactageD / 365.25);
 
-var lastage = "your age is = " + msexactagey + " years " + msexactageM + " Months " + msexactageD + " Days " + msexactageH + " Hours " + msexactageMi + " Minutes " + msexactageS + " Seconds " + " and " + msexactage + " Milliseconds";
+var lastage =
+  "your age is = " +
+  msexactagey +
+  " years " +
+  msexactageM +
+  " Months " +
+  msexactageD +
+  " Days " +
+  msexactageH +
+  " Hours " +
+  msexactageMi +
+  " Minutes " +
+  msexactageS +
+  " Seconds " +
+  " and " +
+  msexactage +
+  " Milliseconds";
 
-if (userage === ""){
-    alert("you didnt enter any input!")
-}else{
-    alert(lastage)
+if (userage === "") {
+  alert("you didnt enter any input!");
+} else {
+  alert(lastage);
 }
