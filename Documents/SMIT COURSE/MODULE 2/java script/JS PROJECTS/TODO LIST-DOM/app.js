9739beae43d9bfe561d7faff329afd190729f73b
Minhaj <minusahab69@gmail.com>
2026-02-24 00:21:41 +0500

    edited todo

--- Documents/SMIT COURSE/MODULE 2/java script/JS PROJECTS/TODO LIST-DOM/app.js
+++ Documents/SMIT COURSE/MODULE 2/java script/JS PROJECTS/TODO LIST-DOM/app.js
@@ -1,5 +1,7 @@
-function addtodo() {
   var container = document.querySelector(".todo-container");
+
+function addtodo() {
+
   var input = document.querySelector(".todo-input");
   var inputValue = input.value;
 
@@ -9,24 +11,48 @@ function addtodo() {
   } 
 
   var todoDiv = document.createElement("div");
-  todoDiv.className = "todo-div";
+  todoDiv.setAttribute("class", "todo-div")
 
-  var todotext = document.createElement("h1");
-  todotext.textContent = inputValue;
+  var todotext = document.createElement("p");
+  todotext.setAttribute("class" , "text")
+  todotext.innerText = inputValue;
 
   var deleteButton = document.createElement("button");
-  deleteButton.textContent = "DELETE TODO";
-  deleteButton.onclick = function () {
-    deletetodo(this);
-  };
+  deleteButton.innerText = "DELETE TODO";
+  deleteButton.setAttribute("class", "delete-button");
+  deleteButton.setAttribute("onclick" , "deletetodo(this)")
+
+  var editButton = document.createElement("button");
+  editButton.innerText = "EDIT TODO";
+  editButton.setAttribute("class", "edit-button");
+  editButton.setAttribute("onclick" , "edittodo(this)") 
 
   todoDiv.appendChild(todotext);
   todoDiv.appendChild(deleteButton);
+  todoDiv.appendChild(editButton);
   container.appendChild(todoDiv);
 
   input.value = "";
 }
 
-function deletetodo(parent) {
-  parent.parentElement.remove();
+function deletetodo(e) {
+  var todoitem = e.parentNode
+  container.removeChild(todoitem);
+
+}
+
+function edittodo(e){
+    var input = document.querySelector(".todo-input");
+    var inputValue = input.value;
+
+    var edititem = e.parentNode
+    var edittext = edititem.querySelector(".text")
+
+    if (inputValue === ""){
+      alert("enter something")
+      return
+    }
+    edittext.innerText = inputValue
+
+    input.value = "";
 }
