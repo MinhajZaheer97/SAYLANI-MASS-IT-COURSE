6b7bd83052f3a2f7d04819d94cc0861035c10300
Minhaj <minusahab69@gmail.com>
2026-05-07 21:12:57 +0500

    excecution context

---
+++ Documents/SMIT COURSE/MODULE 2/java script/js practices/ADVANCED JS/EXCECUTION CONTEXT/app.js
@@ -0,0 +1,15 @@
+// var naam = "Ahmed";  // Global variable
+
+
+// function test() {
+//     if (true) {
+//      var naam = "Ali";  
+//     }
+//     console.log(naam);  // "Ali" — bahar bhi chal gaya! 😱
+// }
+// test();
+
+
+// console.log(naam);
+
+
