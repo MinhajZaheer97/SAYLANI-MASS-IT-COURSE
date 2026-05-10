84998a69257ad2a8f785f2a6150515d8207e6f05
Minhaj <minusahab69@gmail.com>
2026-05-11 00:21:59 +0500

    ..

--- Documents/SMIT COURSE/MODULE 2/java script/js practices/ADVANCED JS/EXCECUTION CONTEXT/app.js
+++ Documents/SMIT COURSE/MODULE 2/java script/js practices/ADVANCED JS/EXCECUTION CONTEXT/app.js
@@ -13,3 +13,27 @@
 // console.log(naam);
 
 
+
+
+
+// function a(num1, num2) {
+//     console.log("A execution")
+
+//     function b() {
+//         console.log("B execution")
+
+//         function c() {
+//             console.log("C execution")
+
+//             console.log(num1 + num2)
+//         }
+
+//         c()
+//     }
+
+//     b()
+
+
+// }
+
+a(5, 10)
