addd39b78d5d73e616045725c4108bcd72fba20e
Minhaj <minusahab69@gmail.com>
2026-07-16 00:23:23 +0500

    signup page with supabase

---
+++ MODULE 2/supabase/signup-page/app.js
@@ -0,0 +1,35 @@
+import { supabase } from './script.js'
+
+const gmailInput = document.getElementById('gmail')
+const usernameInput = document.getElementById('username')
+const passwordInput = document.getElementById('password')
+const signupBtn = document.getElementById('btn')
+
+// console.log('Supabase client ready:', !!supabase)
+
+signupBtn?.addEventListener('click', async() => {
+  console.log({
+    gmail: gmailInput?.value,
+    name: usernameInput?.value,
+    password: passwordInput?.value,
+  })
+
+    await supabase.from('users_data').insert([
+        {
+            name: usernameInput?.value ,
+            gmail: gmailInput?.value,   
+            password: passwordInput?.value,
+        }
+    ]).select()
+
+    const { data, error } = await supabase.auth.signUp({
+        email: gmailInput?.value,
+        password: passwordInput?.value,
+        options: {
+            data: {
+                username: usernameInput?.value
+            }
+        }
+    })
+
+})
