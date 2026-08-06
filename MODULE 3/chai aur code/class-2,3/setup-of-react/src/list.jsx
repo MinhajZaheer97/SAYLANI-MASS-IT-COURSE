a39b3c3fb78b86c98efae36f12891d5b61a76197
Minhaj <minusahab69@gmail.com>
2026-08-06 20:56:41 +0500

    stuydied props and their flow

---
+++ MODULE 3/chai aur code/class-2,3/setup-of-react/src/list.jsx
@@ -0,0 +1,32 @@
+
+function List ({name , age}){
+
+    return(
+        <>
+        <User userName = {name}  userAge = {age}>
+            <ol>
+                <li> name = {name}</li>
+                <li>age = {age}</li>
+            </ol>
+        </User >
+        <User userName = {name}  userAge = {age}>
+            <ol>
+                <li> minu = {name}</li>
+                <li>age = {age}</li>
+            </ol>
+        </User>
+        </>
+    )
+}
+
+function User(data){
+    return(
+        <>
+        {data.children}
+        <p>child = {data.userName}</p>
+        </>
+    )
+}
+
+export {List,User}
+
--- MODULE 3/chai aur code/class-2,3/setup-of-react/src/main.jsx
+++ MODULE 3/chai aur code/class-2,3/setup-of-react/src/main.jsx
@@ -1,9 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import {App} from './App.jsx'
+import {List,User} from './list.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
+    <List name = 'minhaj' age = {16} />
+    {/* <Data/> */}
   </StrictMode>,
 )
