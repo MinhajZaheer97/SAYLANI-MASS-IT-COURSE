f512ab5b5936cee8bd7a8a7fddbba460d12692a3
Minhaj <minusahab69@gmail.com>
2026-08-12 01:29:45 +0500

    created a background changing algorithm using usestate and tailwind

--- MODULE 3/chai aur code/tailwindcss and props/src/components/Navbar.jsx
+++ MODULE 3/chai aur code/tailwindcss and props/src/components/Navbar.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
 import './Navbar.css'
+import { useState } from 'react'
 
 function Navbar() {
-    const btns = ['red', 'blue', 'white', 'purple', 'grey']
+    const [Color , setColor] = useState("white")
+    const btns = ['red', 'blue', 'black', 'purple', 'grey' , 'cyan']
     return (
-        <div className="flex flex-row justify-center items-center absolute bottom-0 left-0 w-full h-20 bg-gray-200">
+        <div className="w-full h-screen flex flex-row justify-center items-end bg-gray-200" 
+        style={{backgroundColor: Color}}>
+
             {
-                btns.map((btn , index) => (
-                    <button className='btn' key={index} onClick={Bgchanger}>{btn}</button>
-                ))
+                btns.map((btn , index)=>{
+                    return <button key={index} className='btn' style={{backgroundColor : btn , color : "white"}} onClick={() => {
+                        setColor(btn)
+                    }}>{btn}</button>
+                })
             }
+
         </div>
     )
 
 }
 
-function Bgchanger(e){
-    e.target.classList.add("bg-green-500")
-    console.log("clicked");
-    
-}
+
 
 export default Navbar
