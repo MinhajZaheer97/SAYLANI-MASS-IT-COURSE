e704c827f741bbf0f2f6d6a689a22c0b6ee5967e
Minhaj Zaheer <minusahab69@gmail.com>
2026-09-25 08:45:27 -0700

    use reducer

--- MODULE 3/chai aur code/useref-usememo-usecontext/src/App.jsx
+++ MODULE 3/chai aur code/useref-usememo-usecontext/src/App.jsx
@@ -1,9 +1,37 @@
+import {useState}from 'react';
+
 const App = () => {
+   const [position, setPosition] = useState({
+    x: 0,
+    y: 0
+  });
   return (
-    <div>
-      <h1>useRef, useMemo, useContext</h1>
+    <div
+      onPointerMove={e => {
+        setPosition({
+          x: e.clientX,
+          y: e.clientY
+        });
+      }}
+      style={{
+        position: 'fixed',
+        inset: 0,
+        height: '100dvh',
+        overflow: 'hidden',
+      }}>
+      <div style={{
+        position: 'absolute',
+        pointerEvents: 'none',
+        backgroundColor: 'red',
+        borderRadius: '50%',
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        left: -10,
+        top: -10,
+        width: 20,
+        height: 20,
+      }} />
     </div>
-  )
+  );
 }
 
 export default App
